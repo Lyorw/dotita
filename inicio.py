8b1e4313025d59348d185ae05ccd51086d8abdba
Lyrow <aguilarrojas8@gmail.com>
2024-09-24 23:06:44 -0500

    hola

---
+++ inicio.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__, template_folder=os.getcwd())
+
+# Ruta para servir el archivo CSS desde la misma carpeta
+@app.route('/diseño.css')
+def serve_css():
+    return send_from_directory(os.getcwd(), 'diseño.css')
+
+# Ruta para servir las imágenes
+@app.route('/imagenes/<path:filename>')
+def serve_images(filename):
+    return send_from_directory(os.path.join(os.getcwd(), 'imagenes'), filename)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
